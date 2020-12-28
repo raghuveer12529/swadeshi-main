5179a1ed79a0ff12a38c4a0f15505ebd12150668
raghuveer12529 <52956789+raghuveer12529@users.noreply.github.com>
2020-12-28 18:38:12 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+// import logo from './logo.svg';
+// import './App.css';
+
+function App() {
+  return (
+    <div className = "grid-container">
+      <header>
+        <a href = "/" >Swadeshi Weavers</a>
+      </header>
+
+      <main>
+        Product List
+      </main>
+
+      <footer>
+        All rights are reserved.
+      </footer>
+    </div>
+   
+  );
+}
+
+export default App;
