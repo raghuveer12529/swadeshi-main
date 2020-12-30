b8d207cfc5037f6a13fd5e9f130f897d4e8846f0
raghuveer12529 <52956789+raghuveer12529@users.noreply.github.com>
2020-12-30 14:42:13 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -2,8 +2,21 @@
 // import './App.css';
 
 //feature-1
+import React from 'react';
+import data from "./data.json";
+import Products from "./components/Products"
 
-function App() {
+class App extends React.Component {
+
+  constructor(){
+    super();
+    this.state = {
+      products : data.products,
+      size :"",
+      sort : ""
+    }
+  }
+  render(){
   return (
     <div className = "grid-container">
       <header>
@@ -11,7 +24,15 @@ function App() {
       </header>
 
       <main>
-        Product List
+        <div className = "content">
+          <div className = " main">
+           <Products products={this.state.products}></Products>
+
+          </div>
+          <div className = "sidebar">
+            Cart Items
+          </div>
+        </div>
       </main>
 
       <footer>
@@ -20,6 +41,7 @@ function App() {
     </div>
    
   );
+}
 }
 
 export default App;
